f1c92ed357707d15d455ad49376a6efa41c6d95f
sapinspys <garri009@fiu.edu>
2020-05-11 12:35:37 -0700

    Begun development, added input and button func

--- rn-first-app/App.js
+++ rn-first-app/App.js
@@ -1,21 +1,39 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, TextInput, Button, StyleSheet } from "react-native";
 
 export default function App() {
-  const [outputText, setOutputText] = useState("Open up App.js to start working on your app!")
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button title="Change Text!" onPress={() => setOutputText("Text changed!")} />
+    // View is similar to <div>
+    // Supported styles found in component docs
+    // VIEWS USE FLEXBOX BY DEFAULT, justifyContent aligns on main axis, alignItems aligns child elements on cross axis
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
     </View>
   );
 }
 
+// StyleSheet = regular object for now
+// In the future StyleSheet will be able to apply styling performance improvements, use it!
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    width: "70%",
+    borderColor: "black",
+    borderWidth: 1,
+    padding: 10,
+  }
 });
