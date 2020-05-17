bf6669e5cac576b7383bf27b7ef26a0dc7689582
sapinspys <garri009@fiu.edu>
2020-05-16 18:59:01 -0700

    Created and implemented basic StartGameScreen

--- guess-a-number-app/App.js
+++ guess-a-number-app/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-import Header from './components/Header';
+import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
     <View style={styles.screen}>
-      <Header title={"Guess a Number"}/>
+      <Header title={"Guess a Number"} />
+      <StartGameScreen />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   screen: {
-    // flex: 1,
+    flex: 1, // ensures component takes all available space
   },
 });
