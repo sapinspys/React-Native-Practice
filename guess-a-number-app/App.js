9517aacd3e3b5e04426fbd86bd28ff07293d1aa1
sapinspys <garri009@fiu.edu>
2020-05-16 18:46:01 -0700

    Begun constructing Header component

--- guess-a-number-app/App.js
+++ guess-a-number-app/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Header from './components/Header';
+
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header title={"hello"}/>
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
