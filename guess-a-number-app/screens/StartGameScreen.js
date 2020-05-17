e6dffb41869b94a27f66e4bce659c1393700f769
sapinspys <garri009@fiu.edu>
2020-05-17 16:31:52 -0700

    Main input drops non-number values on android now

--- guess-a-number-app/screens/StartGameScreen.js
+++ guess-a-number-app/screens/StartGameScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
 import Card from "../components/Card";
@@ -7,6 +7,13 @@ import Input from "../components/Input";
 import Colors from "../constants/colors";
 
 const StartGameScreen = (props) => {
+  const [enteredValue, setEnteredValue]=useState('');
+
+  const numberInputHandler = inputText => {
+    // DROPS NON-NUMBER VALUES
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''))
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>The Game Screen!</Text>
@@ -19,6 +26,8 @@ const StartGameScreen = (props) => {
           autoCorrect={false}
           keyboardType="number-pad"
           maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enteredValue}
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
