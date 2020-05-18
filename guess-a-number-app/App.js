5b6b8b91f8984786b152aac03664e24d5f6eaf7b
sapinspys <garri009@fiu.edu>
2020-05-17 20:25:01 -0700

    Added conditional in to render starting screen first

--- guess-a-number-app/App.js
+++ guess-a-number-app/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import Header from "./components/Header";
@@ -6,11 +6,22 @@ import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber);
+  };
+
+  let content = <StartGameScreen />;
+
+  if (userNumber) {
+    content = <GameScreen />;
+  }
+
   return (
     <View style={styles.screen}>
       <Header title={"Guess a Number"} />
-      <StartGameScreen />
-      <GameScreen />
+      {content}
     </View>
   );
 }
