02d5c8f390351a4e32ead3a116942101fea8c3d3
sapinspys <garri009@fiu.edu>
2020-05-17 20:14:40 -0700

    Begun constructing GameScreen logic

--- guess-a-number-app/screens/GameScreen.js
+++ guess-a-number-app/screens/GameScreen.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {useState} from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 import Colors from "../constants/colors";
 
-const GameScren = props => {
-    return (
-
-    )
+// BUILT OUTSIDE COMPONENT TO AVOID RE-RENDERING, FOR PERFORMANCE (NO PROPS OR STATE USED...)
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // WILL RETURN NUMBER BETWEEN MIN AND MAX
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
 };
 
-const styles = StyleSheet.create({
+const GameScren = (props) => {
+    const [currentGuess, setCurrentGuess] = useState(generateRandomBetween())
+};
 
-});
+const styles = StyleSheet.create({});
 
 export default GameScren;
