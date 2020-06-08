72e4eafcf1d08cd421d3ebf89613ca7af32d2f91
sapinspys <garri009@fiu.edu>
2020-06-07 21:18:15 -0700

    More end game styling

--- guess-a-number-app/screens/GameOverScreen.js
+++ guess-a-number-app/screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Button, StyleSheet, Image } from "react-native";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
 
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
@@ -10,16 +10,22 @@ const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>GAME OVER</TitleText>
-      <View style={styles.imageContainer} >
+      <View style={styles.imageContainer}>
         <Image
           style={styles.image}
-          // source={require("../assets/success.png")}
-          source={{uri: "https://katu.com/resources/media/efd0ef98-3733-455d-8a2e-756d41cfea02-large16x9_MountHood.jpg?1590594687505"}}
+          source={require("../assets/success.png")}
+          // source={{uri: "https://katu.com/resources/media/efd0ef98-3733-455d-8a2e-756d41cfea02-large16x9_MountHood.jpg?1590594687505"}}
           // resizeMode={"contain"} // "COVER" BY DEFAULT
         />
       </View>
-      <BodyText>Number was guessed in {props.rounds} rounds!</BodyText>
-      <BodyText>Your number: {props.userChoice}</BodyText>
+      {/* Text components WILL inherit styling */}
+      <BodyText> 
+        The number was guessed in{" "}
+        <Text style={styles.highlight}>{props.rounds}</Text> round(s)!
+      </BodyText>
+      <BodyText>
+        Your number: <Text style={styles.highlight}>{props.userChoice}</Text>
+      </BodyText>
       <Button
         title="NEW GAME"
         onPress={props.onRestartGame}
@@ -36,17 +42,22 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imageContainer: {
-    width: 300, // WILL NEED RESPONSIVENESS LATER 
+    width: 300, // WILL NEED RESPONSIVENESS LATER
     height: 300,
     borderRadius: 300,
     borderWidth: 2,
     borderColor: "black",
     overflow: "hidden",
-    marginVertical: 30
+    marginVertical: 30,
   },
-  image: { // OVERRIDING AUTOMATIC RESIZING ONLY NEEDED FOR WEB IMAGES
+  image: {
+    // OVERRIDING AUTOMATIC RESIZING ONLY NEEDED FOR WEB IMAGES
     width: "100%",
-    height: "100%"
+    height: "100%",
+  },
+  highlight: {
+    color: Colors.tertiary,
+    fontFamily: "open-sans-bold"
   },
 });
 
