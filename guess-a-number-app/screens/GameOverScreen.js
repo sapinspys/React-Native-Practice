31526c6a77dd871716dd1c7f1d983807f74c06cf
sapinspys <garri009@fiu.edu>
2020-06-07 20:52:49 -0700

    Added and resized end game image

--- guess-a-number-app/screens/GameOverScreen.js
+++ guess-a-number-app/screens/GameOverScreen.js
@@ -4,16 +4,26 @@ import { View, Button, StyleSheet, Image } from "react-native";
 import BodyText from "../components/BodyText";
 import TitleText from "../components/TitleText";
 
-import Colors from "../constants/colors"
+import Colors from "../constants/colors";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <TitleText>GAME OVER</TitleText>
-      <Image source={require('../assets/success.png')} />
+      <View>
+        <Image
+          style={styles.image}
+          source={require("../assets/success.png")}
+          resizeMode={"contain"} // "COVER" BY DEFAULT
+        />
+      </View>
       <BodyText>Number was guessed in {props.rounds} rounds!</BodyText>
       <BodyText>Your number: {props.userChoice}</BodyText>
-      <Button title="NEW GAME" onPress={props.onRestartGame} color={Colors.secondary} />
+      <Button
+        title="NEW GAME"
+        onPress={props.onRestartGame}
+        color={Colors.secondary}
+      />
     </View>
   );
 };
@@ -23,6 +33,15 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    borderRadius: 200,
+    borderWidth: 3,
+    borderColor: "black",
+  },
+  image: {
+    width: "80%",
+    height: 300,
   },
 });
 
